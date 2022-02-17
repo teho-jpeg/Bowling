b721087d58a35fc304796f4c5e2ede76327b3d8f
Teri Homicile <78865551+teho-jpeg@users.noreply.github.com>
2022-02-17 18:59:25 +0000

    2D array - no adding the arrays

--- main.cpp
+++ main.cpp
@@ -9,39 +9,49 @@ int main()
     //bonus rolls go immediately after your tenth frame
      //(three rolls within the last frame or an 11th frame)
    
-    int bowl [10] [2] [3] = {0};
-          //frame //roll //player
+    int player1 [10] [2]= {0};
+             //frame //roll
+    int player2 [10] [2]= {0};
+    int player3 [10] [2]= {0};
+
     int input =0;
     int play1sum =0;
     int play2sum =0;
     int play3sum =0;
    
+
     for (int f=1; f<11; f++)
     {
         cout << "Frame " << f << endl << endl;
-        for (int p=1; p<4; p++)
+
+        cout << "Player 1" << endl;
+        for (int r=1; r<3; r++)
         {
-            cout << "Player " << p << endl;
-            for (int r=1; r<3; r++)
-            {
                 cout << "How may pins did you knock down in Roll " << r << "?\n";
                 cin >> input;
-                bowl[f][r][p] = input;
-            }
-            cout << endl;
+                player1 [f][r] = input;
         }
+        cout << endl;
+
+        cout << "Player 2" << endl;
+        for (int r=1; r<3; r++)
+        {
+                cout << "How may pins did you knock down in Roll " << r << "?\n";
+                cin >> input;
+                player2 [f][r] = input;
+        }
+        cout << endl;
+        
+        cout << "Player 3" << endl;
+        for (int r=1; r<3; r++)
+        {
+                cout << "How may pins did you knock down in Roll " << r << "?\n";
+                cin >> input;
+                player3 [f][r] = input;
+        }
+        cout << endl;
+
     }
 
-    for (int i=0; i<10; i++)
-    {
-        for (int j=1; j<4; j++)
-        {
-            for (int k=1; k<3; k++)
-            {
-                bowl[f][r][p;
-            }
-        }
-    }
-   
     return 0;
 }
