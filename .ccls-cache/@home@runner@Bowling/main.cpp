acf4b3f5d83866daec76f6401b32773fa8c378d2
Teri Homicile <78865551+teho-jpeg@users.noreply.github.com>
2022-02-24 03:50:27 +0000

    output 6x10

--- .ccls-cache/@home@runner@Bowling/main.cpp
+++ .ccls-cache/@home@runner@Bowling/main.cpp
@@ -8,50 +8,45 @@ int main()
 {
     //bonus rolls go immediately after your tenth frame
      //(three rolls within the last frame or an 11th frame)
-   
-    int player1 [10] [2]= {0};
-             //frame //roll
-    int player2 [10] [2]= {0};
-    int player3 [10] [2]= {0};
 
+    int bowl [10] [2] [3] = {0};
+          //frame //roll //player
     int input =0;
     int play1sum =0;
     int play2sum =0;
     int play3sum =0;
    
-
-    for (int f=1; f<11; f++)
+    for (int f=0; f<10; f++)
     {
-        cout << "Frame " << f << endl << endl;
-
-        cout << "Player 1" << endl;
-        for (int r=1; r<3; r++)
+        cout << "Frame " << f+1 << endl << endl;
+        for (int p=0; p<3; p++)
         {
-                cout << "How may pins did you knock down in Roll " << r << "?\n";
+            cout << "Player " << p+1 << endl;
+            for (int r=0; r<2; r++)
+            {
+                cout << "How may pins did you knock down in Roll " << r+1 << "?\n";
                 cin >> input;
-                player1 [f][r] = input;
+                bowl[f][r][p] = input;
+            }
+            cout << endl;
         }
-        cout << endl;
-
-        cout << "Player 2" << endl;
-        for (int r=1; r<3; r++)
-        {
-                cout << "How may pins did you knock down in Roll " << r << "?\n";
-                cin >> input;
-                player2 [f][r] = input;
-        }
-        cout << endl;
-        
-        cout << "Player 3" << endl;
-        for (int r=1; r<3; r++)
-        {
-                cout << "How may pins did you knock down in Roll " << r << "?\n";
-                cin >> input;
-                player3 [f][r] = input;
-        }
-        cout << endl;
-
     }
 
+    for (int f=0; f<10; f++)
+    {
+        for (int p=0; p<3; p++)
+        {
+            if (p=0)
+                for (int r=0; r<2; r++)
+                {
+                    play1sum += bowl[f][r][p];
+                    cout << bowl [f][r][p] << " ";
+                }
+        }
+        cout << endl;
+    }
+
+  cout << endl << play1sum << endl << play2sum << endl << play3sum << endl;
+   
     return 0;
 }
