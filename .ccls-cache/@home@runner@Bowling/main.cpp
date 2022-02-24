93cf7a68d4e1c3f223f2009c60227f3f646492e1
Teri Homicile <78865551+teho-jpeg@users.noreply.github.com>
2022-02-24 21:01:24 +0000

    3 arrays for 3 players

--- .ccls-cache/@home@runner@Bowling/main.cpp
+++ .ccls-cache/@home@runner@Bowling/main.cpp
@@ -1,6 +1,6 @@
 //Bowling Teri Homicile
 //#include <cstdlib>
-#include <iostream>                                                             //delete unnecessary libraries
+#include <iostream>    //delete unnecessary libraries
  
 using namespace std;
 
@@ -8,45 +8,64 @@ int main()
 {
     //bonus rolls go immediately after your tenth frame
      //(three rolls within the last frame or an 11th frame)
+   
+    int player1 [10][2] ={0};
+             //frame //roll
+    int player2 [10][2] ={0};
+    int player3 [10][2] ={0};
 
-    int bowl [10] [2] [3] = {0};
-          //frame //roll //player
-    int input =0;
     int play1sum =0;
     int play2sum =0;
     int play3sum =0;
    
+
     for (int f=0; f<10; f++)
     {
         cout << "Frame " << f+1 << endl << endl;
-        for (int p=0; p<3; p++)
-        {
-            cout << "Player " << p+1 << endl;
-            for (int r=0; r<2; r++)
-            {
-                cout << "How may pins did you knock down in Roll " << r+1 << "?\n";
-                cin >> input;
-                bowl[f][r][p] = input;
-            }
-            cout << endl;
-        }
-    }
 
-    for (int f=0; f<10; f++)
-    {
-        for (int p=0; p<3; p++)
+        cout << "Player 1" << endl;
+        for (int r=0; r<2; r++)
         {
-            if (p=0)
-                for (int r=0; r<2; r++)
-                {
-                    play1sum += bowl[f][r][p];
-                    cout << bowl [f][r][p] << " ";
-                }
+            cout << "How may pins did you knock down in Roll " << r+1 << "?\n";
+            cin >> player1[f][r];
+            play1sum += player1[f][r];
+            if (player1[f][r]==10)
+                r=1;
+            if (player1[f][r-1]==10)
+                play1sum += player1[f][r];
+        }
+        cout << endl;
+
+        cout << "Player 2" << endl;
+        for (int r=0; r<2; r++)
+        {
+            cout << "How may pins did you knock down in Roll " << r+1 << "?\n";
+            cin >> player2[f][r];
+            play2sum += player2[f][r];
+            if (player2[f][r]==10)
+                r=1;
+            if (player2[f][r-1]==10)
+                play2sum += player2[f][r];
+        }
+        cout << endl;
+        
+        cout << "Player 3" << endl;
+        for (int r=0; r<2; r++)
+        {
+            cout << "How may pins did you knock down in Roll " << r+1 << "?\n";
+            cin >> player3[f][r];
+            play3sum += player3[f][r];
+            if (player3[f][r]==10)
+                r=1;
+            if (player3[f][r-1]==10)
+                play3sum += player3[f][r];
         }
         cout << endl;
     }
+    
 
-  cout << endl << play1sum << endl << play2sum << endl << play3sum << endl;
-   
+    cout << "player1 " << play1sum << endl << "player2 " << play2sum << endl << "player3 " << play3sum << endl;
+
+        
     return 0;
 }
